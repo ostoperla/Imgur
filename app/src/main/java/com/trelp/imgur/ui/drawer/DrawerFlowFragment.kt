d56f487390d832dffdfc82fe157b3ae3b42c315a
Sergei Uzenev <ostoperla@gmail.com>
2020-09-04 20:25:35 +0300

    Add layouts for DrawerFlow/NavDrawer Fragments

--- app/src/main/java/com/trelp/imgur/ui/drawer/DrawerFlowFragment.kt
+++ app/src/main/java/com/trelp/imgur/ui/drawer/DrawerFlowFragment.kt
@@ -2,15 +2,27 @@ package com.trelp.imgur.ui.drawer
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.trelp.imgur.R
+import com.trelp.imgur.Screens
 import timber.log.Timber
 
-class DrawerFlowFragment : Fragment() {
+class DrawerFlowFragment : Fragment(R.layout.fragment_drawer_flow) {
 
     //region LifeCycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Timber.d(javaClass.simpleName)
+
+        if (childFragmentManager.fragments.isEmpty()) {
+            childFragmentManager.commit {
+                replace(
+                    R.id.navDrawerContainer,
+                    Screens.NavDrawer.fragment!!
+                )
+            }
+        }
     }
     //endregion
 }
--- app/src/main/java/com/trelp/imgur/ui/drawer/NavDrawerFragment.kt
+++ app/src/main/java/com/trelp/imgur/ui/drawer/NavDrawerFragment.kt
@@ -2,9 +2,10 @@ package com.trelp.imgur.ui.drawer
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.trelp.imgur.R
 import timber.log.Timber
 
-class NavDrawerFragment : Fragment() {
+class NavDrawerFragment : Fragment(R.layout.fragment_nav_drawer) {
 
     //region LifeCycle
     override fun onCreate(savedInstanceState: Bundle?) {
