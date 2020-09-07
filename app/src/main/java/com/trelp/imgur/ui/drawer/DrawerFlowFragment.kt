acb957c4521ff0148e823bb1e7a11adc5df01512
Sergei Uzenev <ostoperla@gmail.com>
2020-09-07 15:16:37 +0300

    Add onBackPressed logic for DrawerFlowFragment

--- app/src/main/java/com/trelp/imgur/ui/drawer/DrawerFlowFragment.kt
+++ app/src/main/java/com/trelp/imgur/ui/drawer/DrawerFlowFragment.kt
@@ -1,12 +1,15 @@
 package com.trelp.imgur.ui.drawer
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.commit
 import com.trelp.imgur.R
 import com.trelp.imgur.Screens
+import com.trelp.imgur.databinding.FragmentDrawerFlowBinding
 import com.trelp.imgur.di.Injector
 import com.trelp.imgur.di.activity.ActivityComponent
 import com.trelp.imgur.di.flow.drawer.DrawerFlowComponent
+import com.trelp.imgur.ui.base.BaseFragment
 import com.trelp.imgur.ui.base.FlowFragment
 import timber.log.Timber
 
@@ -14,6 +17,12 @@ class DrawerFlowFragment : FlowFragment<DrawerFlowComponent>(R.layout.fragment_d
 
     override val container: Int
         get() = R.id.fragmentContainer
+
+    private var fragmentDrawerFlowBinding: FragmentDrawerFlowBinding? = null
+    private val binding get() = fragmentDrawerFlowBinding!!
+
+    private val currentFragment
+        get() = childFragmentManager.findFragmentById(container) as? BaseFragment<*>
 
     //region LifeCycle
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,9 +37,32 @@ class DrawerFlowFragment : FlowFragment<DrawerFlowComponent>(R.layout.fragment_d
                     Screens.NavDrawer.fragment!!
                 )
             }
+            flowRouter.newRootScreen(Screens.Bottom)
         }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentDrawerFlowBinding = FragmentDrawerFlowBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        fragmentDrawerFlowBinding = null
+
+        super.onDestroyView()
+    }
     //endregion
+
+    override fun onBackPressed() {
+        with(binding.drawerLayout) {
+            if (isOpen) {
+                close()
+            } else {
+                currentFragment?.onBackPressed() ?: flowRouter.exit()
+            }
+        }
+    }
 
     //region Dagger
     override fun setupComponent() {
